d9a8964ddd9420246e1555974d2acc44ed05d512
Георгий <jorakl@mail.ru>
2021-11-26 00:02:12 +0300

    Add reference to MediatR.Extensions.Autofac.DependencyInjection

--- src/Tests/Hamster.UseCases.IntegrationTests/Stocks/Queries/GetFundamental/GetFundamentalQueryHandler.cs
+++ src/Tests/Hamster.UseCases.IntegrationTests/Stocks/Queries/GetFundamental/GetFundamentalQueryHandler.cs
@@ -6,6 +6,7 @@ using Autofac;
 using Hamster.Controllers;
 using Hamster.UseCases.Stocks.Queries.GetFundamental;
 using MediatR;
+using MediatR.Extensions.Autofac.DependencyInjection;
 using MediatR.Pipeline;
 using Xunit;
 
@@ -16,7 +17,9 @@ namespace Hamster.UseCases.IntegrationTests.Stocks.Queries.GetFundamental
         [Fact]
         public async void Xcc()
         {
-            var mediator = BuildMediator();
+            var container = BuildContainer();
+            var mediator = container.Resolve<IMediator>();
+            //container.Resolve<StockController>();
             var controller = new StockController(mediator);
             var dto = await controller.Fundamental("FIVE");
             Assert.NotNull(dto);
@@ -34,89 +37,19 @@ namespace Hamster.UseCases.IntegrationTests.Stocks.Queries.GetFundamental
             Assert.Equal(4, dto.RevenueGrowth);
         }
         
-        private static IMediator BuildMediator(/*WrappingWriter writer*/)
+        private static IMediator BuildMediator()
         {
-            var builder = new ContainerBuilder();
-            builder.RegisterAssemblyTypes(typeof(IMediator).GetTypeInfo().Assembly).AsImplementedInterfaces();
-
-            var mediatrOpenTypes = new[]
-            {
-                typeof(IRequestHandler<,>),
-                /*typeof(IRequestExceptionHandler<,,>),
-                typeof(IRequestExceptionAction<,>),
-                typeof(INotificationHandler<>),*/
-            };
-
-            foreach (var mediatrOpenType in mediatrOpenTypes)
-            {
-                builder
-                    .RegisterAssemblyTypes(typeof(Hamster.UseCases.DependencyInjection).GetTypeInfo().Assembly)
-                    .AsClosedTypesOf(mediatrOpenType)
-                    // when having a single class implementing several handler types
-                    // this call will cause a handler to be called twice
-                    // in general you should try to avoid having a class implementing for instance `IRequestHandler<,>` and `INotificationHandler<>`
-                    // the other option would be to remove this call
-                    // see also https://github.com/jbogard/MediatR/issues/462
-                    .AsImplementedInterfaces();
-            }
-
-            //builder.RegisterInstance(writer).As<TextWriter>();
-
-            // It appears Autofac returns the last registered types first
-            /*builder.RegisterGeneric(typeof(RequestPostProcessorBehavior<,>)).As(typeof(IPipelineBehavior<,>));
-            builder.RegisterGeneric(typeof(RequestPreProcessorBehavior<,>)).As(typeof(IPipelineBehavior<,>));
-            builder.RegisterGeneric(typeof(RequestExceptionActionProcessorBehavior<,>)).As(typeof(IPipelineBehavior<,>));
-            builder.RegisterGeneric(typeof(RequestExceptionProcessorBehavior<,>)).As(typeof(IPipelineBehavior<,>));*/
-            /*builder.RegisterGeneric(typeof(GenericRequestPreProcessor<>)).As(typeof(IRequestPreProcessor<>));
-            builder.RegisterGeneric(typeof(GenericRequestPostProcessor<,>)).As(typeof(IRequestPostProcessor<,>));
-            builder.RegisterGeneric(typeof(GenericPipelineBehavior<,>)).As(typeof(IPipelineBehavior<,>));
-            builder.RegisterGeneric(typeof(ConstrainedRequestPostProcessor<,>)).As(typeof(IRequestPostProcessor<,>));
-            builder.RegisterGeneric(typeof(ConstrainedPingedHandler<>)).As(typeof(INotificationHandler<>));*/
-
-            builder.RegisterAssemblyModules(typeof(UseCases.DependencyInjection).Assembly);
-            
-            builder.Register<ServiceFactory>(ctx =>
-            {
-                var c = ctx.Resolve<IComponentContext>();
-                return t => c.Resolve(t);
-            });
-
-            var container = builder.Build();
-
-            // The below returns:
-            //  - RequestPreProcessorBehavior
-            //  - RequestPostProcessorBehavior
-            //  - GenericPipelineBehavior
-            //  - RequestExceptionActionProcessorBehavior
-            //  - RequestExceptionProcessorBehavior
-
-            //var behaviors = container
-            //    .Resolve<IEnumerable<IPipelineBehavior<Ping, Pong>>>()
-            //    .ToList();
-
+            var container = BuildContainer();
             var mediator = container.Resolve<IMediator>();
-
             return mediator;
         }
         
         private static IContainer BuildContainer()
         {
             var builder = new ContainerBuilder();
-
-            builder
-                .RegisterAssemblyTypes(typeof(Hamster.UseCases.DependencyInjection).GetTypeInfo().Assembly)
-                .AsClosedTypesOf(typeof(IRequestHandler<,>))
-                // when having a single class implementing several handler types
-                // this call will cause a handler to be called twice
-                // in general you should try to avoid having a class implementing for instance `IRequestHandler<,>` and `INotificationHandler<>`
-                // the other option would be to remove this call
-                // see also https://github.com/jbogard/MediatR/issues/462
-                .AsImplementedInterfaces();
-            
+            builder.RegisterMediatR(typeof(UseCases.DependencyInjection).GetTypeInfo().Assembly);
             builder.RegisterAssemblyModules(typeof(UseCases.DependencyInjection).Assembly);
-            
             var container = builder.Build();
-
             return container;
         }
     }
