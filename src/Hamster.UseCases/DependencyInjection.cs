7937d5c9f0048a25b08c1f89941eacddde2cf9c9
Георгий <jorakl@mail.ru>
2021-11-25 21:53:46 +0300

    Add reference to Autofac

--- src/Hamster.UseCases/DependencyInjection.cs
+++ src/Hamster.UseCases/DependencyInjection.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using Autofac;
+
+using Hamster.UseCases.Stocks.Queries.GetFundamental;
 
 namespace Hamster.UseCases
 {
-    public class DependencyInjection
+    public class DependencyInjection : Module
     {
+        protected override void Load(ContainerBuilder builder)
+        {
+            RegisterType(builder, typeof(RevenueCalculator));;
+        }
+
+        private static void RegisterType(ContainerBuilder builder, Type type)
+        {
+            builder
+                .RegisterType(type)
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
     }
 }
--- src/Hamster.UseCases/Stocks/Queries/GetFundamental/GetFundamentalQueryHandler.cs
+++ src/Hamster.UseCases/Stocks/Queries/GetFundamental/GetFundamentalQueryHandler.cs
@@ -4,11 +4,28 @@ using MediatR;
 
 namespace Hamster.UseCases.Stocks.Queries.GetFundamental
 {
+    public interface IRevenueCalculator
+    {
+        int GetRevenue(string ticker);
+    }
+
+    public class RevenueCalculator : IRevenueCalculator
+    {
+        public int GetRevenue(string ticker)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    
     internal class GetFundamentalQueryHandler : IRequestHandler<GetFundamentalQuery, FundamentalDto>
     {
+        public GetFundamentalQueryHandler(IRevenueCalculator revenueCalculator)
+        {
+            
+        }
         public Task<FundamentalDto> Handle(GetFundamentalQuery request, CancellationToken cancellationToken)
         {
-            return Task.FromResult(new FundamentalDto());
+            return Task.FromResult(new FundamentalDto{RevenueGrowth = request.Ticker.Length});
         }
     }
 }
