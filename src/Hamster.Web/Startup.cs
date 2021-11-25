5852089bc17fd073150a72919efdf4d85880269c
Георгий <jorakl@mail.ru>
2021-11-25 14:07:04 +0300

    Add Controllers mapping

--- src/Hamster.Web/Startup.cs
+++ src/Hamster.Web/Startup.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Autofac;
-using Hamster.Web.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
+using Hamster.Web.Extensions;
 
 namespace Hamster.Web
 {
@@ -39,8 +35,8 @@ namespace Hamster.Web
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+            { 
+                endpoints.MapControllers();
             });
         }
     }
