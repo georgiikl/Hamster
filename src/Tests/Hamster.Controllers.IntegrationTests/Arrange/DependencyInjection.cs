872f08bd539bb4a278892396503a93220019c525
Георгий <jorakl@mail.ru>
2021-12-03 23:34:04 +0300

    Add arranger into Hamster.Controllers.IntegrationTests

---
+++ src/Tests/Hamster.Controllers.IntegrationTests/Arrange/DependencyInjection.cs
@@ -0,0 +1,21 @@
+using System;
+using Autofac;
+
+namespace Hamster.Controllers.IntegrationTests.Arrange
+{
+    public class DependencyInjection : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //RegisterType(builder, typeof(AlphaVantageAdapter));
+        }
+
+        private static void RegisterType(ContainerBuilder builder, Type type)
+        {
+            builder
+                .RegisterType(type)
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
--- src/Tests/Hamster.Controllers.IntegrationTests/StockController/Fundamental.cs
+++ src/Tests/Hamster.Controllers.IntegrationTests/StockController/Fundamental.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Linq;
-using System.Reflection;
 using System.Threading;
 using Autofac;
-using MediatR.Extensions.Autofac.DependencyInjection;
 using Xunit;
 
-using Hamster.Adapters.Interfaces.AlphaVantage;
+using Hamster.Controllers.IntegrationTests.Arrange;
 
 namespace Hamster.Controllers.IntegrationTests.StockController
 {
@@ -16,7 +12,7 @@ namespace Hamster.Controllers.IntegrationTests.StockController
         public async void All_fundamental_indicators_should_not_be_null()
         {
             // Arrange
-            var controller = BuildContainer().Resolve<Controllers.StockController>();
+            var controller = Arranger.BuildContainer().Resolve<Controllers.StockController>();
             
             // Act
             const string ticker = "PYPL";
@@ -34,53 +30,6 @@ namespace Hamster.Controllers.IntegrationTests.StockController
             Assert.NotNull(dto.Pe);
             Assert.NotNull(dto.Ps);
             Assert.NotNull(dto.EvEbitda);*/
-        }
-
-        [Fact]
-        public async void Foo()
-        {
-            // Arrange
-            var adapter = BuildContainer().Resolve<IAlphaVantageAdapter>();
-            
-            // Act
-            const string ticker = "PYPL";
-            var dto = await adapter.GetIncomeStatement(ticker, CancellationToken.None);
-            
-            // Assert
-            Assert.NotNull(dto);
-            Assert.Equal(ticker, dto.Symbol);
-            Assert.NotNull(dto.AnnualReports);
-            Assert.NotNull(dto.QuarterlyReports);
-            void ItemInspector(IncomeStatementItemDto item)
-            {
-                Assert.Equal("USD", item.ReportedCurrency);
-                var nextDayOfMonth = item.FiscalDateEnding.AddDays(1).Day;
-                Assert.Equal(1, nextDayOfMonth);
-            }
-            var annualReportsInspectors = Enumerable.Repeat<Action<IncomeStatementItemDto>>(ItemInspector, 5).ToArray();
-            Assert.Collection(dto.AnnualReports, annualReportsInspectors);
-            var quarterReportsInspectors = Enumerable.Repeat<Action<IncomeStatementItemDto>>(ItemInspector, 20).ToArray();
-            Assert.Collection(dto.QuarterlyReports, quarterReportsInspectors);
-            
-            Assert.Equal(16933000000, dto.AnnualReports[0].GrossProfit);
-            Assert.Equal(21454000000, dto.AnnualReports[0].TotalRevenue);
-            Assert.Null(dto.AnnualReports[0].InvestmentIncomeNet);
-        }
-        
-        private static IContainer BuildContainer()
-        {
-            var builder = new ContainerBuilder();
-
-            builder
-                .RegisterAssemblyTypes(typeof(Controllers.StockController).GetTypeInfo().Assembly)
-                .AsSelf()
-                .InstancePerLifetimeScope();
-            
-            builder.RegisterMediatR(typeof(UseCases.DependencyInjection).GetTypeInfo().Assembly);
-            builder.RegisterAssemblyModules(typeof(UseCases.DependencyInjection).Assembly);
-            builder.RegisterAssemblyModules(typeof(Adapters.Implementation.DependencyInjection).Assembly);
-            var container = builder.Build();
-            return container;
         }
     }
 }
