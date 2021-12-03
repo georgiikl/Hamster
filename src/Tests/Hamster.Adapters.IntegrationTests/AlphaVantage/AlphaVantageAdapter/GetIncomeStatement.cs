c9ba79268043593a48502b4459d3d615f9a311be
Георгий <jorakl@mail.ru>
2021-12-03 23:28:38 +0300

    Add project Hamster.Adapters.IntegrationTests

---
+++ src/Tests/Hamster.Adapters.IntegrationTests/AlphaVantage/AlphaVantageAdapter/GetIncomeStatement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Autofac;
+using Xunit;
+
+using Hamster.Adapters.IntegrationTests.Arrange;
+using Hamster.Adapters.Interfaces.AlphaVantage;
+
+namespace Hamster.Adapters.IntegrationTests.AlphaVantage.AlphaVantageAdapter
+{
+    public class GetIncomeStatement
+    {
+        [Fact]
+        public async void Should_returns_income_statement()
+        {
+            // Arrange
+            var adapter = Arranger.BuildContainer().Resolve<IAlphaVantageAdapter>();
+            
+            // Act
+            const string ticker = "PYPL";
+            var dto = await adapter.GetIncomeStatement(ticker, CancellationToken.None);
+            
+            // Assert
+            Assert.NotNull(dto);
+            Assert.Equal(ticker, dto.Symbol);
+            Assert.NotNull(dto.AnnualReports);
+            Assert.NotNull(dto.QuarterlyReports);
+            void ItemInspector(IncomeStatementItemDto item)
+            {
+                Assert.Equal("USD", item.ReportedCurrency);
+                var nextDayOfMonth = item.FiscalDateEnding.AddDays(1).Day;
+                Assert.Equal(1, nextDayOfMonth);
+            }
+            var annualReportsInspectors = Enumerable.Repeat<Action<IncomeStatementItemDto>>(ItemInspector, 5).ToArray();
+            Assert.Collection(dto.AnnualReports, annualReportsInspectors);
+            var quarterReportsInspectors = Enumerable.Repeat<Action<IncomeStatementItemDto>>(ItemInspector, 20).ToArray();
+            Assert.Collection(dto.QuarterlyReports, quarterReportsInspectors);
+            
+            Assert.Equal(16933000000, dto.AnnualReports[0].GrossProfit);
+            Assert.Equal(21454000000, dto.AnnualReports[0].TotalRevenue);
+            Assert.Null(dto.AnnualReports[0].InvestmentIncomeNet);
+        }
+    }
+}
---
+++ src/Tests/Hamster.Adapters.IntegrationTests/Arrange/Arranger.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+
+namespace Hamster.Adapters.IntegrationTests.Arrange
+{
+    public static class Arranger
+    {
+        public static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyModules(typeof(Adapters.Implementation.DependencyInjection).Assembly);
+            builder.RegisterAssemblyModules(typeof(DependencyInjection).Assembly);
+            var container = builder.Build();
+            return container;
+        }
+    }
+}
