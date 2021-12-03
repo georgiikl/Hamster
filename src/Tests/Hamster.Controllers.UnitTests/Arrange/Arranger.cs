b08ce6e3d6908482ee2357834e7f22fe158a0939
Георгий <jorakl@mail.ru>
2021-12-03 23:56:50 +0300

    Add project Hamster.Controllers.UnitTests

---
+++ src/Tests/Hamster.Controllers.UnitTests/Arrange/Arranger.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Autofac;
+using MediatR.Extensions.Autofac.DependencyInjection;
+
+namespace Hamster.Controllers.UnitTests.Arrange
+{
+    public static class Arranger
+    {
+        public static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder
+                .RegisterAssemblyTypes(typeof(Controllers.StockController).GetTypeInfo().Assembly)
+                .AsSelf()
+                .InstancePerLifetimeScope();
+            
+            builder.RegisterMediatR(typeof(UseCases.DependencyInjection).GetTypeInfo().Assembly);
+            builder.RegisterAssemblyModules(typeof(UseCases.DependencyInjection).Assembly);
+            //builder.RegisterAssemblyModules(typeof(Adapters.Implementation.DependencyInjection).Assembly);
+            builder.RegisterAssemblyModules(typeof(DependencyInjection).Assembly);
+            var container = builder.Build();
+            return container;
+        }
+    }
+}
---
+++ src/Tests/Hamster.Controllers.UnitTests/StockController/Fundamental.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading;
+using Autofac;
+using Xunit;
+
+using Hamster.Controllers.UnitTests.Arrange;
+
+namespace Hamster.Controllers.UnitTests.StockController
+{
+    public class Fundamental
+    {
+        [Fact]
+        public async void qqqAll_fundamental_indicators_should_not_be_null()
+        {
+            // Arrange
+            var controller = Arranger.BuildContainer().Resolve<Controllers.StockController>();
+            
+            // Act
+            const string ticker = "PYPL";
+            var dto = await controller.Fundamental(ticker, CancellationToken.None);
+            
+            // Assert
+            Assert.NotNull(dto);
+        }
+
+        [Fact]
+        public async void All_fundamental_indicators_should_not_be_null()
+        {
+            // Arrange
+            var controller = Arranger.BuildContainer().Resolve<Controllers.StockController>();
+            
+            // Act
+            const string ticker = "PYPL";
+            var dto = await controller.Fundamental(ticker, CancellationToken.None);
+            
+            // Assert
+            Assert.NotNull(dto);
+            Assert.NotNull(dto.RevenueGrowth);
+            Assert.NotNull(dto.OperatingIncomeGrowth);
+            Assert.NotNull(dto.NetIncomeGrowth);
+            /*Assert.NotNull(dto.GrossMargin);
+            Assert.NotNull(dto.OperatingMargin);
+            Assert.NotNull(dto.NetProfitMargin);
+            Assert.NotNull(dto.Roe);
+            Assert.NotNull(dto.Pe);
+            Assert.NotNull(dto.Ps);
+            Assert.NotNull(dto.EvEbitda);*/
+        }
+    }
+}
